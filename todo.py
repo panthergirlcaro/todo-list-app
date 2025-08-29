21c7353d5ef55d95aec0c23c12a3119cd3a7681b
caroCode <carolynhrobertson@gmail.com>
2025-08-29 12:08:43 -0500

    Update todo.py

--- todo.py
+++ todo.py
@@ -7,7 +7,10 @@ tasks = []
 def add_task(task):
     tasks.append(task)
 
-# Step 3: View tasks
+# Step 3: Print/View tasks
+def view_tasks():
+    for i, task in enumerate(tasks, start=1):
+        print(f"{i}. {task}")
 
 # Step 4: Delete a task
 
@@ -20,9 +23,9 @@ def add_task(task):
 
 # Demo flow (you can run this file directly: python todo.py)
 if __name__ == "__main__":
-    add_task("Finish Cyber 201 assignment")
-    add_task("Push code to GitHub")
-    view_tasks()
-    mark_complete(0)
-    view_tasks()
-    save_tasks()
+     add_task("Finish Cyber 201 assignment")
+     add_task("Push code to GitHub")
+     view_tasks()
+#    mark_complete(0)
+#    view_tasks()
+#    save_tasks()
